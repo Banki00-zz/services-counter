9c7609bd1e2f4d670a8cdd454deef10f40d7466f
Vitaliy <adio9966@gmail.com>
2021-03-27 20:32:14 +0300

    Init

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
 }
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [STATIC_ROOT]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
