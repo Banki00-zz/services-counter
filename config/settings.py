efe506f185b8300b43b76b057aa372bd450e00d1
vitaliy <adio9966@gmail.com>
2021-04-01 17:35:24 +0300

    final

--- config/settings.py
+++ config/settings.py
@@ -106,23 +106,23 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd1o6n4et1u9gev',
-#         'USER': 'ovkzfpqboaluwu',
-#         'PASSWORD': '1be33179503d9118096165a5c39ff73a47a76544d5411b2e0d69cc734813c54c',
-#         'HOST': 'ec2-23-22-191-232.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db',
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1o6n4et1u9gev',
+        'USER': 'ovkzfpqboaluwu',
+        'PASSWORD': '1be33179503d9118096165a5c39ff73a47a76544d5411b2e0d69cc734813c54c',
+        'HOST': 'ec2-23-22-191-232.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db',
+#
+#     }
+# }
 
 LOGIN_URL = 'login'
 
